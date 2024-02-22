78b42094d46d75583da3e2c6de10c2a8387c9359
manibheemagani <160822605+manibheemagani@users.noreply.github.com>
2024-02-22 11:47:15 +0530

    Add files via upload

---
+++ Demo1.java
@@ -0,0 +1,11 @@
+//This program is used to print HI
+import java.io.*;
+class Demo1
+{
+        public static void main ( String args[])
+        {
+         System.out.println("HI");
+         System.out.println("HI"+"\t"+"BYE");
+         System.out.printf("HELLO");
+         }//main
+}//demo
